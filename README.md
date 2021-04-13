bc37817be24be2dbc7fd3eb49816f370cb3fa0fc
joeyoneill <jtoneill@email.sc.edu>
2021-04-13 17:06:43 -0400

    fin

--- README.md
+++ README.md
@@ -4,25 +4,20 @@ Joey O'Neill <oneillj1@g.cofc.edu>
 
 Devin Keirnan <devink500@gmail.com>
 
-Related Experimentation Lab: http://igoro.com/archive/gallery-of-processor-cache-effects/
-
-Another: http://pld.cs.luc.edu/courses/264/spr19/notes/cache.html
-
-
 1. How big is a cache block?
 
-64 bits which can be show with the times at different sized arrays. It is similar times up until 16 meaning it is hitting its limits. 16 integers at 4 bytes a piece = 64 bits.
+64 bits which can be show with the times at different sized arrays. It is similar times up until 16 meaning it is hitting its limits. 16 integers at 4 bytes a piece = 64. This answer can be confirmed with the procfile in which I reference in the output of the code.
 
 
 2. How big is the cache?
 
-The cache is 32k bytes.
+The cache is 32k bytes. This can be seen with the times from the timing_for_cache_and_main() function implemented in our program and displayed by the print_12_to_26() which prints sized of 2^12 to 2^26 which is of sizes 4096 to 67,108,864 byte arrays which since it is an int array, the size of the array is the byte size divided my the sizeof(int) which is 4. Anyways, we can see that the cache (which we are talking about the L1 cache in this instance) sits at 32K bytes which is 2^15 as you can see the first large uptick in from times at size: 16K with a time: 3719203924 ns, then 32K with a time: 3761452094 ns, which are both relatively the same as the times prior shown in the printed output, into 64K which jumps to time: 3983240111 ns and then 128k with 4024861851 ns. 32K is the central point in difference between the time switch meaning it is the end of the cache. This answer can be confirmed with the procfile in which I reference in the output of the code.
 
 
 3. How long does a reference to main memory take?
 
-
+The times begin to level off again at size of bytes 4 million so I will averge the size of 4 million until 64 million which in the instance I am currently run I get an average (mean of the five measurements) time of 887362236 ns for the loop to execute.
 
 4. How long does a reference that can be satisfied from the cache take to complete?
 
-The cache remains relatively the same from array sizes 1-16, and the average (which we use the median from 1000 tries) of each can be taken and added together (times from arrays sized 1, 2, 4, 8), then divided by 4 to get the average cache reference to be completed. In the case I am running while writing this, I recieve a reference average of: 55 ns.
+I averaged (using the mean) caching loop times from 1024 to 32k which are cache based timings that are all relatively the same. On the instance of the run I am currently running I get a time of 373836204 ns. A single reference to array (invidual iteration of a loop) was measuring at 3.2 ns.
--- projectfinal.c
+++ projectfinal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <time.h>  
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
+#include <sys/time.h>
 
 /* Notes from class 
 Timing in C for project 
@@ -94,8 +94,7 @@ void selectionSort(int arr[], int n){
     }
 }
 
-// Main Array for timing experimentation
-int timing(int n) {
+int timing_to_prove_block_size(int n) {
     // time array initialization
     const int time_array_size = 1000;
     int time_array[time_array_size];
@@ -130,33 +129,84 @@ int timing(int n) {
     return median;
 }
 
-void print_timings() {
-    int one = timing(1);
-    int two = timing(2);
-    int four = timing(4);
-    int eight = timing(8);
+// Size of bytes 2^n
+// Times loop accesses
+void timing_for_cache_and_main(int n) {
+    int i;
+    int bytes = 1 << n;     // size in bytes
+    int array_size = bytes / sizeof(int);
+    int *arr = malloc(bytes);  
+
+    // Test Loop 1
+    for (i = 0; i < array_size; i+=1) {
+        arr[i] += 1;
+    }
+
+    // Timing
+    struct timespec start, stop;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    int steps = 64 * 1024 * 1024; // Arbitrary number of steps
+    steps *= 16;
+    int lengthMod = array_size - 1;
+    for (i = 0; i < steps; i++) {
+        arr[(i * 16) & lengthMod]++; // (x & lengthMod) is equal to (x % A.Length)
+    }
+    clock_gettime(CLOCK_MONOTONIC, &stop);
+
+    printf("size: %d --- time: %lld ns\n", 1 << n, ts_usecs(stop) - ts_usecs(start));
+
+    // Free malloc array
+    free(arr);
+}
+
+void print_block_proof_timings() {
+    int one = timing_to_prove_block_size(1);
+    int two = timing_to_prove_block_size(2);
+    int four = timing_to_prove_block_size(4);
+    int eight = timing_to_prove_block_size(8);
     int total = one + two + four + eight;
 
     printf("Time at array size 1: %d ns\n", one);
     printf("Time at array size 2: %d ns\n", two);
     printf("Time at array size 4: %d ns\n", four);
     printf("Time at array size 8: %d ns\n", eight);
-    printf("Time at array size 16: %d ns\n", timing(16));
-    printf("Time at array size 32: %d ns\n", timing(32));
-    printf("Time at array size 64: %d ns\n", timing(64));
-    printf("Time at array size 128: %d ns\n", timing(128));
-    printf("Time at array size 256: %d ns\n", timing(256));
-    printf("Time at array size 512: %d ns\n", timing(512));
-    printf("Time at array size 1024: %d ns\n", timing(1024));
-    printf("Average Cache Reference Timing (mean of timing size 1, 2, 4, and 8): %d ns\n", total/4);
+    printf("Time at array size 16: %d ns\n", timing_to_prove_block_size(16));
+    printf("Time at array size 32: %d ns\n", timing_to_prove_block_size(32));
+    printf("Time at array size 64: %d ns\n", timing_to_prove_block_size(64));
+    printf("Time at array size 128: %d ns\n", timing_to_prove_block_size(128));
+    printf("Time at array size 256: %d ns\n", timing_to_prove_block_size(256));
+    printf("Time at array size 512: %d ns\n", timing_to_prove_block_size(512));
+    printf("Time at array size 1024: %d ns\n", timing_to_prove_block_size(1024));
 }
 
-int main () {
+void print_12_to_26(){
+    printf("\n");
+    // L1 cache
+    timing_for_cache_and_main(12);
+    timing_for_cache_and_main(13);
+    timing_for_cache_and_main(14);
+    // noticable uptick leaving L1 cache still in other oerflow caches
+    timing_for_cache_and_main(15);
+    timing_for_cache_and_main(16);
+    timing_for_cache_and_main(17);
+    timing_for_cache_and_main(18);
+    timing_for_cache_and_main(19);
+    timing_for_cache_and_main(20);
+    timing_for_cache_and_main(21);
+    // Leaving caches and using main memory due to size
+    timing_for_cache_and_main(22);
+    timing_for_cache_and_main(23);
+    timing_for_cache_and_main(24);
+    timing_for_cache_and_main(25);
+    timing_for_cache_and_main(26);
+}
+
+void main(int argc, char **argv) {
     get_cache_block_size();
     get_cache_size();
 
-    print_timings();
+    print_block_proof_timings();
 
-    return 0;
+    print_12_to_26();
 }
-
